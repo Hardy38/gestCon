f43644ae244f244e4fd2c555ddbff29a12e6eec5
hardy NKOUKA <hnkouka@3c-e.com>
2019-10-30 14:16:58 +0100

    Initialoisation du projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask} from '@angular/fire/storage';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gestCon';
+    title = 'gestCon';
+
+    ref: AngularFireStorageReference;
+    task: AngularFireUploadTask;
+
+    constructor(private db: AngularFirestore, private afStorage: AngularFireStorage) {
+        // const things = db.collection('/').valueChanges();
+        // things.subscribe(data => {
+        //     console.log('data: ', data);
+        // });
+    }
+
+    upload(event) {
+        const id = Math.random().toString(36).substring(2);
+        this.ref = this.afStorage.ref(id);
+        this.task = this.ref.put(event.target.files[0]);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+// 1. Import the libs you need
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireStorageModule} from '@angular/fire/storage';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+
+// 2. Add your credentials from step 1
+const config = {
+    apiKey: 'AIzaSyC36ZWYLTyS1gzMNLbw-LYZSQGPzk1_5H4',
+    authDomain: 'poc-gestion-connaissances.firebaseapp.com',
+    databaseURL: 'https://poc-gestion-connaissances.firebaseio.com',
+    projectId: 'poc-gestion-connaissances',
+    storageBucket: 'poc-gestion-connaissances.appspot.com',
+    messagingSenderId: '712204687451',
+    appId: '1:712204687451:web:f2f07742eb77330dd4477e',
+    measurementId: 'G-CQQPL8RXLF'
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+
+        AngularFireModule.initializeApp(config),
+        AngularFirestoreModule, // firestore
+        AngularFireAuthModule, // auth
+        AngularFireStorageModule // storage
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
